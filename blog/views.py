c0945afe004fa28ac34945dd3fc60aa70b6ce6cd
unknown <tomasdiaztoro@gmail.com>
2024-06-27 14:23:15 -0400

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
